c099ac452a01e272796019a4881c3d504282702d
Elina44 <103180214+Elina44@users.noreply.github.com>
2022-05-09 14:55:31 +0200

    import chalk et utilisation du chalk

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const wilderName1 = "Rémi";
+const wilderName2 = "Damien";
+const wilderName3 = "Aurélien";
+const wilderName4 = "Élina";
+
+console.log(wilderName1, wilderName2, wilderName3, wilderName4);
+console.log(chalk.red(wilderName1));
+console.log(chalk.blue(wilderName1));
+console.log(chalk.green("Aurélien"));
